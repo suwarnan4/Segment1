bfa0e1134ce327df7a8b031e69d7540a031b475f
Suwarnan <suwarnan@gmail.com>
2015-07-02 01:29:07 +0530

    Segment1 commit...

---
+++ Segment1/Segment1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Segment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("SupportTraining");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Support");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Training");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
